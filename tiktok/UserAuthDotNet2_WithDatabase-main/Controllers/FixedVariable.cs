e63110be6abf9ce86f3748d9cd90d094c45ed383
Sandeep Rawat <132456591+sandeep-narayana@users.noreply.github.com>
2023-09-22 13:43:28 +0530

    temporary del fixed

---
+++ tiktok/UserAuthDotNet2_WithDatabase-main/Controllers/FixedVariable.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace UserAuthDotBet2_WithDatabase;
+
+
+public class FixedVariable : ControllerBase
+{
+    public static string publicKey = "74ee164f928bf76ccf3d062136efb7fd";
+    public static string privateKey = "ac0ca953653a282f684db5c0d142a056";
+
+}
